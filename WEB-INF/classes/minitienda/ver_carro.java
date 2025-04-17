86580191a0fc76be04924ddaba2988b903452005
candela <rcandelab@gmail.com>
2025-04-17 18:03:20 +0200

    ver carrito

--- WEB-INF/classes/minitienda/ver_carro.java
+++ WEB-INF/classes/minitienda/ver_carro.java
@@ -1,108 +1,93 @@
-package supercalculadora;
+package minitienda;
 
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
 import java.io.*;
 import java.util.*;
-
-/** Servlet para almacenar CDs seleccionados por el usuario */
-
-public class SumaNumeros extends HttpServlet {
-
-    // Variables privadas a la instancia del Servlet
-    private int sumaInstanciaServlet = 0;
-
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-      throws ServletException, IOException {
-      // Generamos un objeto sesion
-      HttpSession session = request.getSession(true);
-
-      // Generamos un objeto para el contexto de la aplicacion
-      ServletContext context = getServletContext();
-
-      // Almacenamos los parametros de entrada en variables temporales
-      String numeroString = request.getParameter("numero");      
-      // Convertimos "numero" a entero
-      int numeroActual = Integer.parseInt(numeroString);
-	  
-      // Recuperamos sumaSesion de la sesion y si no existe, creamos este atributo
-      Integer sumaSesion = (Integer)session.getAttribute("sumaSesion");
-      if ( sumaSesion == null )
-	  {
-	      System.out.println("Sesion es null");
-	  
-	      // Inicializamos el atributo sumaSesion
-	      session.setAttribute("sumaSesion", new Integer(0) );
-	      sumaSesion = (Integer)session.getAttribute("sumaSesion");
-	  }
-
-      // Recuperamos contadorContext del Contexto y si no existe, creamos este atributo
-      // contadorContext cuenta el numero de operaciones realizadas en la supercalculadora
-      // COMPLETAR...
-      Integer sumaContext = (Integer)context.getAttribute("sumaContext");
-      if ( sumaContext == null )
-	  {
-	      System.out.println("Context es null");
-	  
-	      // Inicializamos el atributo sumaSesion
-	      context.setAttribute("sumaContext", new Integer(0) );
-	      sumaContext = (Integer)context.getAttribute("sumaContext");
-	  }
-
-      // Incrementamos la variable global del servlet
-      // COMPLETAR...   
-      sumaInstanciaServlet+=numeroActual;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 
-      // Incrementamos la variable de la sesion
-      // COMPLETAR...   
-      sumaSesion+=numeroActual;
+public class ver_carro extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        HashMap<String, Integer> carrito = (HashMap<String, Integer>) session.getAttribute("carrito");
 
-      // Incrementamos en uno el contador del contexto
-      // COMPLETAR...   
-      sumaContext+=1;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><head><title>Carrito de Compras</title></head><body bgcolor=\"#FDF5E6\">");
+
+        // Si el carrito no está vacío, mostrar los productos
+        if (carrito != null && carrito.size() > 0) {
+            out.println("<h2 align=\"center\" border=\"1\" width=\"100%\" style=\"background-color: #ffffff;\">");
+            out.println("<tr><th>Titulo del CD</th><th>Cantidad</th><th style=\"width: 100px;\">Importe</th><th style=\"width: 80px;\">Eliminar</th></tr>");
+
+            float total = 0;
+            String[] keys = carrito.keySet().toArray(new String[carrito.size()]);
+            for (int i = 0; i < carrito.size(); i++) {
+                String item = keys[i];
+                int cantidad = carrito.get(item);
+                StringTokenizer t = new StringTokenizer(descripcionCD,"|");
+                t.nextToken();
+                t.nextToken();
+                t.nextToken();
+                precioString = t.nextToken();
+                precioString = precioString.replace('$',' ').trim();
+
+                float precio = Float.parseFloat(precioString);
+                float importe = precio * cantidad;
+                total += importe;
+
+                out.println("<tr><td>" + descripcionCD + "</th><td>" + cantidad + "</td><td style=\"width: 100px;\">" + importe + " euros</td><td style=\"width: 80px;\">")
+                out.println("<form action=\"ver_carro\" method=\"POST\">");
+                out.println("<input type=\"hidden\" name=\"descripcionCD\" value=\"" + descripcionCD +"\">");
+                out.println("<input type=\"submit\" value=\"Eliminar\"></form>");
+                out.println("</th></tr>");
+            }
+
+            // Importe final
+            out.println("<tr><td></td>");
+            out.println("<td>IMPORTE TOTAL</td><td>" + total + " euros</td><td></td></tr></table>")
+        }
+
+        else {
+            out.println("<h2>Carrito de Compras Vacio</h2>");
+            out.prinltn("<p>No hay productos en el carrito de compras.</p>");
+            out.println("<p>Para agregar productos, visite la <a href=\"index.html\">Tienda</a>.</p>");
+        }
 
 
-      // Almacenamos la suma en la sesion
-      session.setAttribute("sumaSesion", new Integer( sumaSesion ));
+        // Imágenes con enlaces para seguir comprando (tienda) o pagar (caja)
+        out.println("<div style=\"display: flex; justify-content: center;\">");
+        out.println("<figure style=\"text-align: center; margin-right: 20px;\">");
+        out.println("<a href=\"index.html\"><img src=\"../../../imagenes/carrito_compra.gif\" alt=\"Seguir comprando\"></a><figcaption><a href=\"index.html\">Seguir comprando</a></figcaption></figure>");
 
-      // Almacenamos la suma en el contexto
-      // COMPLETAR...   
-      context.setAttribute("sumaContext", new Integer( sumaContext ));
+        out.println("<figure style=\"text-align: center; margin-left: 20px;\">");
+        out.println("<a href=\"ver_caja\"><img src=\"../../../imagenes/pago_importe2.jpg\" alt=\"Me largo a pagar\"></a><figcaption><a href=\"ver_caja\">Me largo a pagar</a></figcaption></figure>");
+
+        out.println("</div></body></html>");
+    }
 
 
-      // Generamos pagina de salida
-      // COMPLETAR PRESENTACION DE RESULTADOS...
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
-      String docType =
-	  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
-	  "Transitional//EN\">\n";
-      out.println(docType +
-		  "<HTML>\n" +
-		  "<HEAD><TITLE>Suma Numeros: La calculadora definitiva</TITLE></HEAD>\n" +
-		  "<BODY BGCOLOR=\"#FDF5E6\">\n" +
-		  "<center> <H1>Suma Numeros: La calculadora definitiva </H1>\n" +
-		  "\n\n" +
-		  "<table border=\"0\" cellpadding=\"0\" width=\"75%\" bgcolor=\"#FFFFFF\">" +
-		  "<tr>" +
-		  "<td><b>Numero introducido</b></td>" +
-		  "<td><b>sumaInstanciaServlet</b></td>" +
-		  "<td><b>sumaSesion</b></td>" +
-		  "<td><b>Contador operaciones en Context</b></td>" +
-		  "</tr>" +
-		  "<tr>" +
-		  "<td><b>" + numeroActual + "</b></td>" +
-		  "<td><b>" + sumaInstanciaServlet + "</b></td>" +
-		  "<td><b>" + sumaSesion + "</b></td>" +
-		  "<td><b>" + sumaContext + "</b></td>" +
-		  "</tr>" +
-		  "</table>" +
-		  "<p>" +
-		  "<a HREF=\"/supercalculadora/index.html\">Introducir mas numeros</a>" +
-		  "</center>" +
-		  "</BODY></HTML>");
-  }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String descripcionCD = request.getParameter("descripcionCD");
+        if (descripcionCD != null) {
+            HttpSession session = request.getSession(true);
+            HashMap<String, Integer> carrito = (HashMap<String, Integer>) session.getAttribute("carrito");
+            int cantidad = carrito.get(descripcionCD);
+
+            if (cantidad <= 1) {
+                carrito.remove(descripcionCD);
+            }
+
+            else {
+                cantidad = cantidad - 1;
+                carrito.put(descripcionCD, cantidad);
+            }
+
+            session.setAttribute("carrito", carrito);
+
+            response.sendRedirect("ver_carro");
+            return;
+        }
+    }
 }
