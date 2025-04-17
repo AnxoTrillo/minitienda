b3dd0c78c6634f470a5cc98e2127e242b16b3de9
AnxoTrillo <anxo.trillo@rai.usc.es>
2025-04-17 17:35:25 +0200

    subo anhadir_cd

--- WEB-INF/classes/minitienda/anhadir_cd.java
+++ WEB-INF/classes/minitienda/anhadir_cd.java
@@ -1,4 +1,4 @@
-package supercalculadora;
+package minitienda;
 
 
 import jakarta.servlet.*;
@@ -8,7 +8,8 @@ import java.util.*;
 
 /** Servlet para almacenar CDs seleccionados por el usuario */
 
-public class SumaNumeros extends HttpServlet {
+
+public class anhadir_cd extends HttpServlet {
 
     // Variables privadas a la instancia del Servlet
     private int sumaInstanciaServlet = 0;
@@ -16,93 +17,34 @@ public class SumaNumeros extends HttpServlet {
   public void doGet(HttpServletRequest request,
                     HttpServletResponse response)
       throws ServletException, IOException {
-      // Generamos un objeto sesion
-      HttpSession session = request.getSession(true);
-
+      
       // Generamos un objeto para el contexto de la aplicacion
-      ServletContext context = getServletContext();
+      ServletContext session = request.getSession(true);
 
-      // Almacenamos los parametros de entrada en variables temporales
-      String numeroString = request.getParameter("numero");      
-      // Convertimos "numero" a entero
-      int numeroActual = Integer.parseInt(numeroString);
-	  
-      // Recuperamos sumaSesion de la sesion y si no existe, creamos este atributo
-      Integer sumaSesion = (Integer)session.getAttribute("sumaSesion");
-      if ( sumaSesion == null )
+      // se mete la cadena seleccionada en una variable
+      String descripcionCD = request.getParameter("CD");
+      String cantidad = Integer.parseInt(request.getParameter("cantidad"));
+
+      Map<String, Integer> carrito= (Map<String, Integer>)session.getAttribute("carrito");
+      if ( carrito == null )
 	  {
-	      System.out.println("Sesion es null");
+	      System.out.println("Carrito es null");
 	  
 	      // Inicializamos el atributo sumaSesion
-	      session.setAttribute("sumaSesion", new Integer(0) );
-	      sumaSesion = (Integer)session.getAttribute("sumaSesion");
+	      context.setAttribute("carrito", new HashMap<>() );
 	  }
 
-      // Recuperamos contadorContext del Contexto y si no existe, creamos este atributo
-      // contadorContext cuenta el numero de operaciones realizadas en la supercalculadora
-      // COMPLETAR...
-      Integer sumaContext = (Integer)context.getAttribute("sumaContext");
-      if ( sumaContext == null )
-	  {
-	      System.out.println("Context es null");
-	  
-	      // Inicializamos el atributo sumaSesion
-	      context.setAttribute("sumaContext", new Integer(0) );
-	      sumaContext = (Integer)context.getAttribute("sumaContext");
-	  }
-
-      // Incrementamos la variable global del servlet
-      // COMPLETAR...   
-      sumaInstanciaServlet+=numeroActual;
-
-
-      // Incrementamos la variable de la sesion
-      // COMPLETAR...   
-      sumaSesion+=numeroActual;
-
-      // Incrementamos en uno el contador del contexto
-      // COMPLETAR...   
-      sumaContext+=1;
-
-
-      // Almacenamos la suma en la sesion
-      session.setAttribute("sumaSesion", new Integer( sumaSesion ));
+      
+      if(carrito.containsKey(descripcionCD)){
+        Integer temp=carrito.get(descripcionCD);
+        carrito.put(descripcionCD, temp+cantidad);
+      }else{
+        carrito.put(descripcionCD, cantidad);
+      }
 
       // Almacenamos la suma en el contexto
       // COMPLETAR...   
-      context.setAttribute("sumaContext", new Integer( sumaContext ));
+      session.setAttribute("carrito", new Map<String, Integer>(carrito));
 
-
-      // Generamos pagina de salida
-      // COMPLETAR PRESENTACION DE RESULTADOS...
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
-      String docType =
-	  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
-	  "Transitional//EN\">\n";
-      out.println(docType +
-		  "<HTML>\n" +
-		  "<HEAD><TITLE>Suma Numeros: La calculadora definitiva</TITLE></HEAD>\n" +
-		  "<BODY BGCOLOR=\"#FDF5E6\">\n" +
-		  "<center> <H1>Suma Numeros: La calculadora definitiva </H1>\n" +
-		  "\n\n" +
-		  "<table border=\"0\" cellpadding=\"0\" width=\"75%\" bgcolor=\"#FFFFFF\">" +
-		  "<tr>" +
-		  "<td><b>Numero introducido</b></td>" +
-		  "<td><b>sumaInstanciaServlet</b></td>" +
-		  "<td><b>sumaSesion</b></td>" +
-		  "<td><b>Contador operaciones en Context</b></td>" +
-		  "</tr>" +
-		  "<tr>" +
-		  "<td><b>" + numeroActual + "</b></td>" +
-		  "<td><b>" + sumaInstanciaServlet + "</b></td>" +
-		  "<td><b>" + sumaSesion + "</b></td>" +
-		  "<td><b>" + sumaContext + "</b></td>" +
-		  "</tr>" +
-		  "</table>" +
-		  "<p>" +
-		  "<a HREF=\"/supercalculadora/index.html\">Introducir mas numeros</a>" +
-		  "</center>" +
-		  "</BODY></HTML>");
   }
 }
