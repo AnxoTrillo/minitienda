bafb5f011be6069caf8016b15d4a3dcc7db69cb7
candela <rcandelab@gmail.com>
2025-05-10 17:18:59 +0200

    JSP (con scriplets) listo

--- WEB-INF/classes/minitienda/confirmarCompra.java
+++ WEB-INF/classes/minitienda/confirmarCompra.java
@@ -6,9 +6,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class confirmarCompra extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             HttpSession session = request.getSession(true);
-            HashMap<String, Integer> carrito = new HashMap<String, Integer>();
+            HashMap<String, Integer> carrito = new HashMap<>();
             session.invalidate();
 
             response.sendRedirect("index.html");
